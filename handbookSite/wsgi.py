5d8a7b1b9fac7bed0ac41fca8ee07d887aa47a46
AidanaAzaMatova <30952073+AidanaAzaMatova@users.noreply.github.com>
2022-06-07 02:59:14 +0600

    Update wsgi.py

--- handbookSite/wsgi.py
+++ handbookSite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handbookSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aidana123456.pythonanywhere.com.settings')
 
 application = get_wsgi_application()
